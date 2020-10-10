16d103f76a1b17d6f869e624c868a1b27519ac97
Christian Van Loon <66512995+WarMachine-282@users.noreply.github.com>
2020-10-09 22:25:53 -0700

    update

---
+++ week3/PokemonList/index.js
@@ -0,0 +1,20 @@
+const xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://api.vschool.io/pokemon", true);
+xhr.send();
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    const JSONdata = xhr.responseText;
+    const data = JSON.parse(JSONdata);
+    console.log(data);
+    showData(data.objects[0].pokemon);
+  }
+};
+function showData(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const h1 = document.createElement("h1");
+    h1.textContent = arr[i].name;
+    document.body.appendChild(h1);
+  }
+}
